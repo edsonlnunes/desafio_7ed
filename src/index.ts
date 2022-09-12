9bd0491215fdbfad6d0160d2e1e9636313c7f07b
Vinicius Menezes <viniemil@gmail.com>
2022-09-12 20:11:10 -0300

    vinicius commit

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
-import { Aluno } from "./Aluno"
+import { Aluno } from "./Aluno";
 
-const listaAlunos=[
-    new Aluno("Kelly", 25),
-    new Aluno("Paulo", 35),
-    new Aluno("Rafael", 28),
-    new Aluno("Edson", 25),
-    new Aluno("Fabio", 35),
-    new Aluno("Diener", 29),
-    new Aluno("Vinicius", 35),
-]
+const listaAlunos = [
+  new Aluno("Kelly", 25),
+  new Aluno("Paulo", 35),
+  new Aluno("Rafael", 28),
+  new Aluno("Edson", 25),
+  new Aluno("Fabio", 35),
+  new Aluno("Diener", 29),
+  new Aluno("Vinicius", 35),
+];
 
-const gabarito = ['A', 'C', 'B', 'A', 'C', 'A', 'B', 'C', 'A', 'B']
-function GabaritoNumero(){
-    const random = Math.random()
-    if(random <= 0.33)
-    {
-        console.log('A')
-    }
-    else if(random >= 0.66)
-    {
-        console.log('C')
-    }
-    else{
-        console.log('B')
-    }
-} 
+const gabarito = ["A", "C", "B", "A", "C", "A", "B", "C", "A", "B"];
+
+function gerarResposta() {
+  const random = Math.random();
+  if (random <= 0.33) {
+    return "A";
+  } else if (random >= 0.66) {
+    return "C";
+  } else {
+    return "B";
+  }
+}
+
+listaAlunos.forEach((aluno) => {
+  gabarito.forEach(questao);
+});
