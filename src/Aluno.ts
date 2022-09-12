1439af90e3481fccb00c8620b51234adb794e8c7
Paulo Pires <ppiresdev@gmail.com>
2022-09-12 19:34:22 -0300

    Primeiro commit

---
+++ src/Aluno.ts
@@ -0,0 +1,21 @@
+// Aluno com os atributos Nome, Idade, Nota e Status.
+
+class Aluno {
+  private _nome: string;
+  private _idade: number;
+  private _nota: number;
+  private _status: string = "";
+
+  constructor(nome: string, idade: number, nota: number) {
+    this._nome = nome;
+    this._idade = idade;
+    this._nota = nota;
+  }
+
+  get nome(): string {
+    return this._nome;
+  }
+  get idade(): number {
+    return this._idade;
+  }
+}
